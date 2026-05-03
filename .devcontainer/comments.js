502cbe1bbdaba1f1107eda3bc66bb291325d4c04
KOLLURI VENKATA SAI HARSHITHA <bujjiluharshikolluri@gmail.com>
2026-05-03 12:23:58 +0000

    Copilot third commit

---
+++ .devcontainer/comments.js
@@ -0,0 +1,8 @@
+// Create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+server.listen(3000, 'localhost');
